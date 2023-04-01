8875c81514772f36b19dbd5b599ebaf6d26390ec
georgiagrayland <georgiamayeva@live.co.uk>
2023-04-01 10:52:13 +0000

    add extra records to new table

--- sql-crud.py
+++ sql-crud.py
@@ -80,13 +80,22 @@ tim_berners_lee = Programmer(
     famous_for="World Wide Web"
 )
 
+georgia_grayland = Programmer(
+    first_name="Georgia",
+    last_name="Grayland",
+    gender="F",
+    nationality="British",
+    famous_for="Nothing"
+)
+
 # add each instance of our programmers to our session
 # session.add(ada_lovelace)
-session.add(alan_turing)
-session.add(grace_hopper)
-session.add(margaret_hamilton)
-session.add(bill_gates)
-session.add(tim_berners_lee)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+session.add(georgia_grayland)
 
 # commit our session to the database
 session.commit()
